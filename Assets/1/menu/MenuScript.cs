197aa0c037356c7ce139b0e650db913d28bd9d64
Kotryna <kaimelisvy24@outlook.com>
2016-07-05 17:40:17 +0200

    menu scene

---
+++ Assets/1/menu/MenuScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+
+    public void LoadScene(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+    public static MenuScript instance;
+
+    public static MenuScript GetInstance()
+    {
+        return instance;
+    }
+}
